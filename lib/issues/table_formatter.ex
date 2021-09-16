5ff83d8e5ef100f6beba9e9e62a617d5f5496c50
w40141 <w.40141@gmail.com>
2021-09-16 09:51:37 +0900

    add doc_test

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -1,5 +1,5 @@
-defmodule TableFormatter do
-  import Enum, only: [each: 2, map: 2, map_join: 3, max: 2]
+defmodule Issues.TableFormatter do
+  import Enum, only: [each: 2, map: 2, map_join: 3, max: 1]
 
   @doc """
   Takes a list of row data, where each row is a Map, and a list
@@ -25,12 +25,10 @@ defmodule TableFormatter do
   each column. The `headers` parameter contains the list
   of columns to extract.
   ## Example
-    iex> list = [
-                  Enum.into([{"a", "1"}, {"b", "2"}, {"c", "3"}], %{}),
-                  Enum.into([{"a", "4"}, {"b", "5"}, {"c", "6"}], %{})
-                ]
-    iex> Issues.TableFormatter.split_into_columns(list, ["a", "b", "c"])
-    [ [ "1", "4" ], [ "2", "5" ], [ "3", "6" ] ]
+  iex> list = [Enum.into([{"a", "1"}, {"b", "2"}, {"c", "3"}], %{}),
+  ...>         Enum.into([{"a", "4"}, {"b", "5"}, {"c", "6"}], %{})]
+  iex> Issues.TableFormatter.split_into_columns(list, ["a", "b", "c"])
+  [ [ "1", "4" ], [ "2", "5" ], [ "3", "6" ] ]
   """
   def split_into_columns(rows, headers) do
     for header <- headers do
@@ -50,29 +48,50 @@ defmodule TableFormatter do
   def printable(str), do: to_string(str)
 
   @doc """
-  Given a list con
+  Given a list containing sublists, where each sublist contains the data for
+  a column, return a list containing the maximum width of each column.
+  ## Example
+    iex> data = [ [ "cat", "wombat", "elk" ], [ "mongoose", "and", "gnu" ] ]
+    iex> Issues.TableFormatter.widths_of(data)
+    [ 6, 8 ]
   """
   def widths_of(columns) do
-    for column <- columns do
-      column
-      |> Enum.map(&String.length/1)
-      |> Enum.max()
-    end
+    for column <- columns, do: column |> map(&String.length/1) |> max
   end
 
+  @doc """
+  Return a format string that hard-codes the widths of a set of columns.
+  We put `" | "` between each column.
+  ## Example
+    iex> width = [ 5, 6, 99 ]
+    iex> Issues.TableFormatter.format_for(width)
+    "~-5s | ~-6s | ~-99s~n"
+  """
   def format_for(column_widths) do
-    Enum.map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
+    map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
   end
 
+  @doc """
+  Generate the line that goes below the column headings. It is a string of
+  hyphens, with + signs where the vertical bar between the column goes.
+  ## Example
+    iex> width = [ 5, 6, 9 ]
+    iex> Issues.TableFormatter.separator(width)
+    "------+--------+----------"
+  """
   def separator(column_widths) do
-    Enum.map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
+    map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
   end
 
+  @doc """
+  Given a list containing rows of data, a list containing the header selectors,
+  and a format string, write the extracted data under control of the format string.
+  """
   def puts_in_columns(data_by_columns, format) do
     data_by_columns
     |> List.zip()
-    |> Enum.map(&Tuple.to_list/1)
-    |> Enum.each(&puts_one_line_in_columns(&1, format))
+    |> map(&Tuple.to_list/1)
+    |> each(&puts_one_line_in_columns(&1, format))
   end
 
   def puts_one_line_in_columns(fields, format) do
